303eb341954b1ecb3d6db956cc71be5f002f6cfd
Udayendra Pal <109019105+Udayendra@users.noreply.github.com>
2024-03-06 22:29:23 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,64 @@
+// tooggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+}
+
+// scroll section
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            // active navbar links 
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+            // active sections for animateion on scroll
+            sec.classList.add('show-animate');
+        }
+
+        else {
+            sec.classList.remove('show-animate');
+
+        }
+    });
+
+    // sticky header
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon and navbar when click navbar links (scroll)
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+}
+
+// link contact form to google sheet
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxNM6hgUeOn5-oY_nzmjwE8RRePNhDPkIyICvcDJkq0SN4ESnNU-ZbcvEG1wqCD6Xki/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Message sent successfully"
+            setTimeout(function () {
+                msg.innerHTML = ""
+            }, 5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+})
